4303510d466a4a724ea6d6e759ac3335073c0e25
Ishita <ishitagogoi8@gmail.com>
2026-02-17 23:54:21 +0530

    UC3 - Refactored banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,35 +1,13 @@
-<<<<<<< HEAD
-public static void main(String[] args) {
-
-    System.out.println("*   *  ****  ****  ****");
-    System.out.println("*   *  *  *  *  *  *  *");
-    System.out.println("*   *  ****  ****  ****");
-
-}
-/**
- * OOPSBannerApp UC1 - OOPS Banner Display Application
- *
- * This class demonstrates a simple Java application that displays the Object
- * Oriented Programming System OOPS acronym to the console.
- *
- * @author Ishita
- * @version 1.0
- */
-
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
-=======
-public class OOPSBannerApp {
-    public static void main(String[] args) {
 
-        System.out.println("*********************");
-        System.out.println("*     WELCOME TO    *");
-        System.out.println("*        O O P S     *");
-        System.out.println("*********************");
-
->>>>>>> feature/UC2-EnhanceBanner
+        System.out.println(String.join(" ", " *** ", " *** ", " ***** ", " ***** "));
+        System.out.println(String.join(" ", "*   *", "*   *", "*     *", "*      "));
+        System.out.println(String.join(" ", "*   *", "*   *", "*     *", "*      "));
+        System.out.println(String.join(" ", "*   *", "*   *", "***** ", " ***** "));
+        System.out.println(String.join(" ", "*   *", "*   *", "*     ", "     * "));
+        System.out.println(String.join(" ", "*   *", "*   *", "*     ", "     * "));
+        System.out.println(String.join(" ", " *** ", " *** ", "*     ", " ***** "));
     }
 }
-
